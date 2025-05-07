fc2770c6f2bff2273fd8fe6ee2d37cb85dc301ee
Sg0lens <samuel.ssgms@gmail.com>
2025-05-07 16:49:19 -0300

    Remove unnecessary blank lines in main.py

--- main.py
+++ main.py
@@ -10,8 +10,6 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 produto_repo.criar_tabela()
 
-
-
 @app.get("/")
 async def root():
     protudos = produto_repo.obter_todos()
