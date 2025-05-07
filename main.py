1b896afd1e6a105c24a4a734bc3b20851870f459
Sg0lens <samuel.ssgms@gmail.com>
2025-05-07 16:42:58 -0300

    Refactor product display in index.html and initialize product table in main.py

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from data import produto_repo
 import uvicorn
 
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
+produto_repo.criar_tabela()
+
 
 
 @app.get("/")
 async def root():
-    response = templates.TemplateResponse("index.html", {"request": {}})
+    protudos = produto_repo.obter_todos()
+    response = templates.TemplateResponse("index.html", {"request": {}, "produtos": protudos})
     return response
 
 
